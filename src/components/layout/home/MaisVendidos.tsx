1096a10f3180e0688dd2facd2e69fda2d756008d
izzyadora <souzad.isadora@gmail.com>
2026-08-22 14:39:08 -0300

    style: aumenta gap da seção "mais vendidos"

--- src/components/layout/home/MaisVendidos.tsx
+++ src/components/layout/home/MaisVendidos.tsx
@@ -6,7 +6,7 @@ export default async function MaisVendidos() {
   const produtos = await getMaisVendidos();
 
   return (
-    <section className="flex flex-col gap-4 w-full p-6 bg-white border-b border-chocolate/20">
+    <section className="flex flex-col gap-4 w-full p-6 pt-20 bg-white border-b border-chocolate/20">
       {/* Cabeçalho */}
       <div className="flex flex-col gap-2 max-w-2xl mx-auto text-center">
         <p className="font-dm-sans text-xs sm:text-sm text-militar-300 tracking-[0.15em] font-black uppercase">
