579a0fd6f13172204aed8615907e22813b201316
izzyadora <souzad.isadora@gmail.com>
2026-08-22 14:53:41 -0300

    style: estiliza página de busca

--- src/components/layout/busca/BuscaComponent.tsx
+++ src/components/layout/busca/BuscaComponent.tsx
@@ -1,18 +1,22 @@
 import Link from "next/link";
 import BuscaAuxiliar from "./BuscaAuxiliar";
+import { HeartCrack } from "lucide-react";
 
 export default function Busca() {
   const count = 0;
 
   return (
-    <div className="bg-offwhite w-full font-dm-sans">
-      <BuscaAuxiliar count={count} />
+    <div className="bg-offwhite w-full font-dm-sans p-12">
+      <div className="flex items-center justify-center">
+        <BuscaAuxiliar count={count} />
+      </div>
       {count === 0 ? (
-        <div className="w-full flex flex-col">
-          <span className="text-2xl font-semibold">Nenhum item encontrado</span>
-          <span>
+        <div className="w-full flex flex-col items-center justify-center p-24">
+          <HeartCrack className="w-16 h-16 text-chocolate"></HeartCrack>
+          <span className="text-2xl font-semibold font-source-serif text-chocolate">Nenhum item encontrado</span>
+          <span className="text-chocolate/70">
             Tente refinar sua busca, ou explore{" "}
-            <Link href="/produtos" className="text-militar-300">nossos produtos</Link>.
+            <Link href="/produtos" className="text-militar-300 font-bold">nossos produtos</Link>.
           </span>
         </div>
       ) : (
