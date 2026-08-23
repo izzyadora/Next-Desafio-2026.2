f8800b5ef59f3299676791d8c9f9fe9f3bbd89e9
izzyadora <souzad.isadora@gmail.com>
2026-08-23 20:45:31 -0300

    fix: corrige importação do prisma na route post de login

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,12 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/src/lib/db";
 
-export async function POST(req: NextRequest){
-    try{   
-        const{name, email, password} = await req.json()
-        
-        PrismaClient().catch(err => NextResponse.json(err))
-    } catch{
+export async function POST(req: NextRequest) {
+  try {
+    const { name, email, password } = await req.json();
 
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Usuário não encontrado" }, { status: 404 });
     }
+
+    return NextResponse.json({ message: "Login realizado com sucesso", user });
+  } catch (err) {
+    return NextResponse.json({ error: "Erro ao processar requisição", details: err }, { status: 500 });
+  }
 }
